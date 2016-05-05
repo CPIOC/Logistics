3cdedeb235ffe024d6563cf475618651791cd4d9
cf0566 <cf0566@qq.com>
2016-05-05 17:55:33 +0800

    加一些工具

--- logistics/src/main/java/com/cpic/taylor/logistics/activity/HomeActivity.java
+++ logistics/src/main/java/com/cpic/taylor/logistics/activity/HomeActivity.java
@@ -2,19 +2,30 @@ package com.cpic.taylor.logistics.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.cpic.taylor.logistics.R;
 import com.cpic.taylor.logistics.base.BaseActivity;
+import com.cpic.taylor.logistics.fragment.HomeLineFragment;
+import com.cpic.taylor.logistics.fragment.HomePoliceFragment;
+import com.cpic.taylor.logistics.fragment.HomeRoadFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Taylor on 2016/5/4.
  */
-public class HomeActivity extends BaseActivity{
+public class HomeActivity extends BaseActivity {
 
     // 记录上次点击返回键的时间
     private long lastTime;
@@ -22,6 +33,16 @@ public class HomeActivity extends BaseActivity{
     private DrawerLayout layout;
     private ImageView ivMine;
     private TextView tvChat;
+    private RadioGroup rgroup;
+    private RadioButton lastButton;
+    // Fragment的管理类
+    private FragmentManager mManager;
+    // Fragment的事务类
+    private FragmentTransaction mTrans;
+    // 管理Fragment的List集合
+    private List<Fragment> mFragList;
+    public String curFragmentTag = "";
+
 
     @Override
     protected void getIntentData(Bundle savedInstanceState) {
@@ -38,11 +59,28 @@ public class HomeActivity extends BaseActivity{
         layout = (DrawerLayout) findViewById(R.id.activity_home_drawerlayout);
         ivMine = (ImageView) findViewById(R.id.activity_home_iv_mine);
         tvChat = (TextView) findViewById(R.id.activity_home_chat);
+        rgroup = (RadioGroup) findViewById(R.id.activity_home_rgroup);
+        lastButton = (RadioButton) findViewById(R.id.activity_home_rbtn_line);
     }
 
     @Override
     protected void initData() {
+        initFragment();
+    }
 
+    private void initFragment() {
+        // TODO Auto-generated method stub
+        mFragList = new ArrayList<Fragment>();
+        mFragList.add(new HomeLineFragment());
+        mFragList.add(new HomePoliceFragment());
+        mFragList.add(new HomeRoadFragment());
+
+        mManager = getSupportFragmentManager();
+        mTrans = mManager.beginTransaction();
+        lastButton.setChecked(true);
+        mTrans.add(R.id.activity_home_framelayout, mFragList.get(0), "0");
+        mTrans.show(mFragList.get(0));
+        mTrans.commit();
     }
 
     @Override
@@ -56,16 +94,45 @@ public class HomeActivity extends BaseActivity{
         tvChat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,ChatMainActivity.class);
+                Intent intent = new Intent(HomeActivity.this, ChatMainActivity.class);
                 startActivity(intent);
             }
         });
+
+        rgroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // 当前选中的radioButton
+                RadioButton selectRbtn = (RadioButton) findViewById(checkedId);
+
+                int index = Integer.parseInt(selectRbtn.getTag().toString());
+                int lastIndex = Integer.parseInt(lastButton.getTag().toString());
+
+                Fragment mFragment = mManager.findFragmentByTag(index + "");
+
+                mTrans = mManager.beginTransaction();
+
+                if (mFragment == null) {
+                    mTrans.add(R.id.activity_home_framelayout, mFragList.get(index), "" + index);
+                }
+
+                // 设置界面隐藏与显示，避免一次性加载所有界面
+                mTrans.show(mFragList.get(index));
+                mTrans.hide(mFragList.get(lastIndex));
+                mTrans.commit();
+
+                lastButton = selectRbtn;
+            }
+        });
     }
+
     @Override
     public void onBackPressed() {
         // 获取本次点击的时间
         long currentTime = System.currentTimeMillis();
         long dTime = currentTime - lastTime;
+
         if (dTime < 2000) {
             finish();
         } else {
---
+++ logistics/src/main/java/com/cpic/taylor/logistics/fragment/HomeLineFragment.java
@@ -0,0 +1,280 @@
+package com.cpic.taylor.logistics.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.route.BusRouteResult;
+import com.amap.api.services.route.DrivePath;
+import com.amap.api.services.route.DriveRouteResult;
+import com.amap.api.services.route.RouteSearch;
+import com.amap.api.services.route.WalkRouteResult;
+import com.cpic.taylor.logistics.R;
+import com.cpic.taylor.logistics.overlay.DrivingRouteOverlay;
+import com.cpic.taylor.logistics.utils.AMapUtil;
+
+/**
+ * Created by Taylor on 2016/4/29.
+ */
+public class HomeLineFragment extends Fragment implements LocationSource,
+        AMapLocationListener, AMap.OnMapClickListener,
+        AMap.OnMarkerClickListener, AMap.OnInfoWindowClickListener, AMap.InfoWindowAdapter, RouteSearch.OnRouteSearchListener {
+    //定位
+    private MapView mapView;
+    private AMap aMap;
+    private OnLocationChangedListener mListener;
+    private AMapLocationClient mlocationClient;
+    private AMapLocationClientOption mLocationOption;
+
+    //路径规划
+    private RouteSearch mRouteSearch;
+    private DriveRouteResult mDriveRouteResult;
+    private LatLonPoint mStartPoint = new LatLonPoint(30.917636, 116.397743);//起点，
+    private LatLonPoint mEndPoint = new LatLonPoint(30.984947, 116.494689);//终点，
+
+    private RadioGroup mGPSModeGroup;
+
+
+    private TextView tvStart,tvStop;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home_line, null);
+        mapView = (MapView) view.findViewById(R.id.map);
+        tvStart = (TextView) view.findViewById(R.id.fragment_line_tv_start);
+        tvStop = (TextView) view.findViewById(R.id.fragment_line_tv_stop);
+
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        init(view);
+        setfromandtoMarker();
+        registerListener();
+
+        return view;
+    }
+
+    private void registerListener() {
+        tvStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "出发地", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tvStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "目的地", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private void setfromandtoMarker() {
+        aMap.addMarker(new MarkerOptions()
+                .position(AMapUtil.convertToLatLng(mStartPoint))
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.start)));
+        aMap.addMarker(new MarkerOptions()
+                .position(AMapUtil.convertToLatLng(mEndPoint))
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.end)));
+        searchRouteResult(1, RouteSearch.DrivingDefault);
+    }
+
+    private void init(View view) {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+            setUpMap();
+        }
+        mRouteSearch = new RouteSearch(getActivity());
+        mRouteSearch.setRouteSearchListener(this);
+    }
+
+    /**
+     * 开始搜索路径规划方案
+     */
+    public void searchRouteResult(int routeType, int mode) {
+        if (mStartPoint == null) {
+            Toast.makeText(getActivity(), "正在定位中", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (mEndPoint == null) {
+            Toast.makeText(getActivity(), "终点未设置", Toast.LENGTH_SHORT).show();
+        }
+        final RouteSearch.FromAndTo fromAndTo = new RouteSearch.FromAndTo(
+                mStartPoint, mEndPoint);
+
+        RouteSearch.DriveRouteQuery query = new RouteSearch.DriveRouteQuery(fromAndTo, mode, null,
+                null, "");// 第一个参数表示路径规划的起点和终点，第二个参数表示驾车模式，第三个参数表示途经点，第四个参数表示避让区域，第五个参数表示避让道路
+        mRouteSearch.calculateDriveRouteAsyn(query);// 异步路径规划驾车模式查询
+    }
+
+    /**
+     * 设置一些amap的属性
+     */
+    private void setUpMap() {
+        aMap.setLocationSource(this);// 设置定位监听
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        // 设置定位的类型为定位模式 ，可以由定位、跟随或地图根据面向方向旋转几种
+        aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+        deactivate();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+        if (null != mlocationClient) {
+            mlocationClient.onDestroy();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (mListener != null && aMapLocation != null) {
+            if (aMapLocation != null
+                    && aMapLocation.getErrorCode() == 0) {
+                mListener.onLocationChanged(aMapLocation);// 显示系统小蓝点
+            } else {
+                String errText = "定位失败," + aMapLocation.getErrorCode() + ": " + aMapLocation.getErrorInfo();
+                Log.i("oye", errText);
+            }
+        }
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener = onLocationChangedListener;
+        if (mlocationClient == null) {
+            mlocationClient = new AMapLocationClient(getActivity());
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位监听
+            mlocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            mlocationClient.startLocation();
+        }
+    }
+
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+    }
+
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void onBusRouteSearched(BusRouteResult busRouteResult, int i) {
+
+    }
+
+    @Override
+    public void onDriveRouteSearched(DriveRouteResult driveRouteResult, int i) {
+        if (i == 1000) {
+            if (driveRouteResult != null && driveRouteResult.getPaths() != null) {
+                if (driveRouteResult.getPaths().size() > 0) {
+                    mDriveRouteResult = driveRouteResult;
+                    final DrivePath drivePath = mDriveRouteResult.getPaths()
+                            .get(0);
+                    DrivingRouteOverlay drivingRouteOverlay = new DrivingRouteOverlay(
+                            getActivity(), aMap, drivePath,
+                            mDriveRouteResult.getStartPos(),
+                            mDriveRouteResult.getTargetPos());
+                    drivingRouteOverlay.removeFromMap();
+                    drivingRouteOverlay.addToMap();
+                    drivingRouteOverlay.zoomToSpan();
+                    int dis = (int) drivePath.getDistance();
+                    int dur = (int) drivePath.getDuration();
+                    String des = AMapUtil.getFriendlyTime(dur) + "(" + AMapUtil.getFriendlyLength(dis) + ")";
+
+                } else if (driveRouteResult != null && driveRouteResult.getPaths() == null) {
+                    Toast.makeText(getActivity(), "没有结果", Toast.LENGTH_SHORT).show();
+                }
+
+            } else {
+                Toast.makeText(getActivity(), "没有结果", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getActivity(), "没有结果", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onWalkRouteSearched(WalkRouteResult walkRouteResult, int i) {
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+    }
+}
