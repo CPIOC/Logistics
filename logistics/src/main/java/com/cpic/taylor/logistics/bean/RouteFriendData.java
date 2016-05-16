604a3e2e2fe6ae7797993853740a19351a4cccdf
nattyboy <1149140370@qq.com>
2016-05-16 09:29:11 +0800

    路况信息代码---9;24----wiser

---
+++ logistics/src/main/java/com/cpic/taylor/logistics/bean/RouteFriendData.java
@@ -0,0 +1,94 @@
+package com.cpic.taylor.logistics.bean;
+
+/**
+ * Created by xuan on 2016/5/16.
+ */
+public class RouteFriendData {
+
+    private  String id;
+    private  String user_id;
+    private  String user_name;
+    private  String content;
+    private  String cat_id;
+    private  String address;
+    private  String created_at;
+    private  String user_img;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCat_id() {
+        return cat_id;
+    }
+
+    public void setCat_id(String cat_id) {
+        this.cat_id = cat_id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUser_img() {
+        return user_img;
+    }
+
+    public void setUser_img(String user_img) {
+        this.user_img = user_img;
+    }
+
+    @Override
+    public String toString() {
+        return "RouteFriendData{" +
+                "id='" + id + '\'' +
+                ", user_id='" + user_id + '\'' +
+                ", user_name='" + user_name + '\'' +
+                ", content='" + content + '\'' +
+                ", cat_id='" + cat_id + '\'' +
+                ", address='" + address + '\'' +
+                ", created_at='" + created_at + '\'' +
+                ", user_img='" + user_img + '\'' +
+                '}';
+    }
+}
