e584fc00e67308ec4c8290bfdcb84755f5539229
cf0566 <cf0566@qq.com>
2016-05-17 11:34:09 +0800

    城市列表修改

--- logistics/src/main/java/com/cpic/taylor/logistics/activity/ChooseAreaActivity.java
+++ logistics/src/main/java/com/cpic/taylor/logistics/activity/ChooseAreaActivity.java
@@ -46,7 +46,6 @@ public class ChooseAreaActivity extends BaseActivity implements PoiSearch.OnPoiS
 
     private int screenWidth, screenHight;
     private ListView lvArea;
-    private ArrayList<String> datas;
     private AreaAdapter adapter;
     private TextView tvLine;
     private TextView tvCancel;
@@ -145,8 +144,7 @@ public class ChooseAreaActivity extends BaseActivity implements PoiSearch.OnPoiS
         tvArea.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                openPopwin();
-                Intent intent = new Intent(ChooseAreaActivity.this,MainActivity.class);
+                Intent intent = new Intent(ChooseAreaActivity.this,CityPickerActivity.class);
                 startActivityForResult(intent,1);
             }
         });
@@ -197,40 +195,7 @@ public class ChooseAreaActivity extends BaseActivity implements PoiSearch.OnPoiS
 
     }
 
-    private void openPopwin() {
-        View view = View.inflate(ChooseAreaActivity.this, R.layout.popupwindow_area, null);
-        popupWindow = new PopupWindow(view, screenWidth, screenHight / 2);
-        popupWindow.setFocusable(true);
-        lvArea = (ListView) view.findViewById(R.id.pop_area_lv);
 
-        datas = new ArrayList<>();
-        AreaData();
-        adapter = new AreaAdapter();
-        adapter.setDatas(datas);
-        lvArea.setAdapter(adapter);
-
-        WindowManager.LayoutParams params = ChooseAreaActivity.this.getWindow().getAttributes();
-        ChooseAreaActivity.this.getWindow().setAttributes(params);
-        popupWindow.setBackgroundDrawable(new ColorDrawable());
-        popupWindow.setOutsideTouchable(false);
-        popupWindow.showAsDropDown(tvLine);
-        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
-            @Override
-            public void onDismiss() {
-                WindowManager.LayoutParams params = ChooseAreaActivity.this.getWindow().getAttributes();
-                params.alpha = 1f;
-                getWindow().setAttributes(params);
-            }
-        });
-
-        lvArea.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                tvArea.setText(datas.get(i));
-                popupWindow.dismiss();
-            }
-        });
-    }
 
     @Override
     public void onGetInputtips(List<Tip> list, int rCode) {
@@ -391,43 +356,10 @@ public class ChooseAreaActivity extends BaseActivity implements PoiSearch.OnPoiS
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1){
+        if (requestCode == 1&&data != null){
             tvArea.setText(data.getStringExtra("city"));
         }
 
     }
-
-    private void AreaData() {
-        datas.add("北京市");
-        datas.add("天津市");
-        datas.add("上海市");
-        datas.add("重庆市");
-        datas.add("河北省");
-        datas.add("河南省");
-        datas.add("云南省");
-        datas.add("辽宁省");
-        datas.add("黑龙江省");
-        datas.add("湖南省");
-        datas.add("安徽省");
-        datas.add("新疆维吾尔");
-        datas.add("广西壮族");
-        datas.add("湖北省");
-        datas.add("甘肃省");
-        datas.add("山西省");
-        datas.add("内蒙古");
-        datas.add("陕西省");
-        datas.add("吉林省");
-        datas.add("福建省");
-        datas.add("贵州省");
-        datas.add("广东省");
-        datas.add("西藏");
-        datas.add("四川省");
-        datas.add("宁夏回族");
-        datas.add("海南省");
-        datas.add("台湾省");
-        datas.add("香港特别行政区");
-        datas.add("澳门特别行政区");
-    }
-
 
 }
